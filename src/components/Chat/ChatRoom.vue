b503e2a214783e3eada1984196dff23aa75bdba0
Tuan Tran Van <tuantv.sw@gmail.com>
2024-12-20 01:16:10 +0700

    test UI

--- src/components/Chat/ChatRoom.vue
+++ src/components/Chat/ChatRoom.vue
@@ -227,7 +227,7 @@ export default {
 
       this.autoLoadInterval = setInterval(() => {
         this.loadPrivateMessages(receiverUserId);
-      }, 1000);
+      }, 5000);
     },
 
     stopAutoLoadMessages() {
